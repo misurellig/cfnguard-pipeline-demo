681bd9ee971dc72412adfbaca48364c07f629a75
misurellig <giuseppe.misurelli@gmail.com>
2021-01-14 20:17:30 +0100

    dependency and codecommit

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ import os
 from aws_cdk import core
 
 from cfnguard_pipeline_demo.cfnguard_pipeline_demo_stack import CfnguardPipelineDemoStack
+from cfnguard_pipeline_demo.roles_permissions_stack import RolesPermissionsStack
 
 
 app = core.App()
-CfnguardPipelineDemoStack(app, "cfnguard-pipeline-demo", env=core.Environment(
+ns = CfnguardPipelineDemoStack(app, "cfnguard-pipeline-demo", env=core.Environment(
     account=os.environ["CDK_DEFAULT_ACCOUNT"],
     region=os.environ.get("CDK_DEPLOY_REGION", os.environ["CDK_DEFAULT_REGION"]))
 )
+
+roles_permissions = RolesPermissionsStack(app, "RolesPermissionsStack")
+roles_permissions.add_dependency(ns)
 
 app.synth()
--- cfnguard_pipeline_demo/cfnguard_pipeline_demo_stack.py
+++ cfnguard_pipeline_demo/cfnguard_pipeline_demo_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import (
     core,
-    aws_iam as iam
+    aws_iam as iam,
+    aws_codecommit as codecommit
 )
 
 
@@ -10,82 +11,10 @@ class CfnguardPipelineDemoStack(core.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
-        cloud_formation_role = iam.Role(self, "CloudFormationCfnGuardDemo",
-            assumed_by=iam.ServicePrincipal("codepipeline.amazonaws.com"),
-            description="CodePipeline role with trusted relationship for CFN"
+        repo = codecommit.Repository(self, "Repository",
+            repository_name="cfnguard-demo",
+            description="CFN code to demo at the checkride."
         )
 
-        cloud_formation_role.add_to_policy(iam.PolicyStatement(
-            sid="AllowCreateServiceLinkedRoleWithConditions",
-            resources=["*"],
-            actions=["iam:CreateServiceLinkedRole"],
-            conditions={
-                "StringEquals":{
-                    "iam:AWSServiceName":[
-                        "autoscaling.amazonaws.com",
-                        "ec2scheduled.amazonaws.com",
-                        "elasticloadbalancing.amazonaws.com"
-                    ]
-                }
-            }
-        ))
-
-        cloud_formation_role.add_to_policy(iam.PolicyStatement(
-            sid="AllowCloudFormationDoItsJob",
-            resources=["*"],
-            actions=[
-                "s3:GetObjectAcl",
-                "s3:GetObject",
-                "cloudwatch:*",
-                "ec2:*",
-                "autoscaling:*",
-                "s3:List*",
-                "s3:HeadBucket"
-            ]
-        ))
-
-        code_pipeline_role = iam.Role(self, "CodePipelineCfnGuardDemo",
-            assumed_by=iam.ServicePrincipal("cloudformation.amazonaws.com"),
-            description="CodePipeline role with trusted relationship for CloudFormation"
-        )
-
-        code_pipeline_role.add_to_policy(iam.PolicyStatement(
-            sid="AllowPassRoleWithConditions",
-            resources=["*"],
-            actions=["iam:PassRole"],
-            conditions={
-                "StringEqualsIfExists":{
-                    "iam:PassedToService":["cloudformation.amazonaws.com","ec2.amazonaws.com"]
-                }
-            }
-        ))
-
-        code_pipeline_role.add_to_policy(iam.PolicyStatement(
-            sid="AllowCodePipelineDoItsJob",
-            resources=["*"],
-            actions=[
-                "codecommit:UploadArchive",
-                "codecommit:CancelUploadArchive",
-                "codecommit:GetCommit",
-                "codecommit:GetUploadArchiveStatus",
-                "codecommit:GetBranch",
-                "codestar-connections:UseConnection",
-                "codebuild:BatchGetBuilds",
-                "codedeploy:CreateDeployment",
-                "codedeploy:GetApplicationRevision",
-                "codedeploy:RegisterApplicationRevision",
-                "codedeploy:GetDeploymentConfig",
-                "codedeploy:GetDeployment",
-                "codebuild:StartBuild",
-                "codedeploy:GetApplication",
-                "s3:*",
-                "cloudformation:*",
-                "ec2:*"
-            ]
-        ))
-
-        core.CfnOutput(self, "CloudFormationRoleArn", value=cloud_formation_role.role_arn)
-        core.CfnOutput(self, "CloudFormationRoleId", value=cloud_formation_role.role_id)
-        core.CfnOutput(self, "CodePipelineRoleArn", value=code_pipeline_role.role_arn)
-        core.CfnOutput(self, "CodePipelineRoleId", value=code_pipeline_role.role_id)
+        core.CfnOutput(self, "RepositoryCloneUrlGrc", value=repo.repository_clone_url_grc)
     
